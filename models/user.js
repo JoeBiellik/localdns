385a9d3f2ad64f49888f0fcb0daee3cf26e904e7
Mathieu Lomax <mat.lomax@gmail.com>
2016-04-03 19:19:27 +0100

    Change name to email

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 var user = new mongoose.Schema({
-	name: { type: String, unique: true },
+	email: { type: String, unique: true },
 	sub: { type: String, unique: true },
-	ip: { type: String, unique: true }
+	ip: { type: String }
 }, {
 	timestamps: true
 });
