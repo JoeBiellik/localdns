1d04e857e1f87f6a8711628ce7b0f122678931f6
Mathieu Lomax <mat.lomax@gmail.com>
2016-04-03 18:49:20 +0100

    Serve static files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'jade');
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
 	res.render('index', { title: 'Test', text: 'Hello World!'});
