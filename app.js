de2bd3c132040fd7e421f424d339936b405ef153
Joe Biellik <joebiellik@gmail.com>
2016-04-03 19:48:00 +0100

    Add HTTP basic auth

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
+var auth = require('http-auth');
 var app = express();
+
+var authMiddleware = auth.connect(auth.basic({
+	realm: 'SUPER SECRET STUFF'
+}, function (username, password, callback) {
+	callback(username == 'admin' && password == 'password');
+}));
 
 app.set('view engine', 'jade');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
 	res.render('index', { title: 'Test', text: 'Hello World!'});
+});
+
+app.get('/secret', authMiddleware, function (req, res) {
+    res.send('Authenticated');
 });
 
 module.exports = app;
