15fd6766d509f9637e5b95406f0075088a545869
Joe Biellik <joebiellik@gmail.com>
2016-04-03 19:08:37 +0100

    Only return A records

--- dns-server.js
+++ dns-server.js
@@ -4,7 +4,17 @@ var dnsd = require('dnsd');
 var User = require('./models/user');
 
 var server = dnsd.createServer(function(req, res) {
-	res.end('1.2.3.4');
+	var question = res.question && res.question[0];
+
+	// Only allow A record requests
+	if (question.type != 'A') return res.end();
+
+	console.log('A lookup for domain: %s', question.name);
+
+	res.answer.push({'name': question.name, 'type': 'A', 'data': '1.2.3.4'})
+	res.answer.push({'name': question.name, 'type': 'A', 'data': '2.3.4.5'})
+
+	return res.end()
 });
 
 server.listen(process.env.PORT || config.dns.port, process.env.HOST || config.dns.host);
