23697e632ca89a4adb8cff25310b781bcd40de46
Mathieu Lomax <mat.lomax@gmail.com>
2016-04-05 23:11:50 +0100

    Improve default records

--- dns-server.js
+++ dns-server.js
@@ -4,52 +4,37 @@ var dnsd = require('dnsd');
 var db = require('./db')();
 var User = require('./models/user');
 
-var typeA = function (res, name) {
-
-};
-
 var server = dnsd.createServer(function(req, res) {
 	var question = res.question && res.question[0];
 
-	// Only allow A record requests
-	// if (question.type != 'A') {
-	// 	res.responseCode = 5;
-	// 	return res.end();
-	// }
-
-	console.log('A lookup for domain: %s', question.name);
-
-	// var u = new User({
-	// 	email: 'mat.lomax@gmail.com',
-	// 	sub: 'mat',
-	// 	ip: '2.4.6.8',
-	// 	password: 'password'
-	// });
-	//
-	// u.save();
+	console.log('%s lookup for domain: %s', question.name, question.type);
 
 	if (question.name !== config.domain && !question.name.endsWith('.' + config.domain)) {
 		res.responseCode = 5; // Refused
 		return res.end();
 	}
 
-	if (question.name === config.domain || question.name === 'www.' + config.domain) {
-		switch (question.type) {
-			case 'A':
-				res.answer.push({ 'name': question.name, 'type': 'A', 'data': config.ip });
-				break;
-			case 'NS':
-				res.answer.push({ 'name': question.name, 'type': 'NS', 'data': 'ns1.' + config.domain });
-				break;
-			case 'CNAME':
-				res.answer.push({ 'name': question.name, 'type': 'CNAME', 'data': config.domain });
-				res.answer.push({ 'name': question.name, 'type': 'A', 'data': config.ip });
-				break;
-			default:
-				res.answer.push({ 'name': question.name, 'type': 'NS', 'data': 'ns1.' + config.domain });
-				res.answer.push({ 'name': question.name, 'type': 'CNAME', 'data': config.domain });
-				res.answer.push({ 'name': question.name, 'type': 'A', 'data': config.ip });
-		}
+	if (question.name === config.domain || question.name === 'www.' + config.domain || question.name === 'ns1.' + config.domain) {
+		config.dns.records.forEach(function(record) {
+			var name = record.name ? record.name + '.' + config.domain : config.domain;
+			if (name === question.name) {
+				if ((question.type !== 'A' && question.type !== 'NS') || record.type === question.type) {
+					res.answer.push({ 'name': name, 'type': record.type, 'data': record.data });
+				}
+			}
+		});
+
+		// switch (question.type) {
+		// 	case 'A':
+		// 		res.answer.push({ 'name': question.name, 'type': 'A', 'data': config.ip });
+		// 		break;
+		// 	case 'NS':
+		// 		res.answer.push({ 'name': question.name, 'type': 'NS', 'data': 'ns1.' + config.domain });
+		// 		break;
+		// 	case '*':
+		// 		res.answer.push({ 'name': question.name, 'type': 'NS', 'data': 'ns1.' + config.domain });
+        //         res.answer.push({ 'name': question.name, 'type': 'A', 'data': config.ip });
+		// }
 
 		return res.end();
 	}
@@ -59,14 +44,14 @@ var server = dnsd.createServer(function(req, res) {
 			if (!err && doc) {
 				switch (question.type) {
 					case 'A':
-						res.answer.push({ 'name': question.name, 'type': 'A', 'data': config.ip });
+						res.answer.push({ 'name': question.name, 'type': 'A', 'data': doc.ip });
 						break;
 					case 'NS':
 						res.answer.push({ 'name': question.name, 'type': 'NS', 'data': 'ns1.' + config.domain });
 						break;
-					default:
+					case '*':
 						res.answer.push({ 'name': question.name, 'type': 'NS', 'data': 'ns1.' + config.domain });
-						res.answer.push({ 'name': question.name, 'type': 'A', 'data': config.ip });
+						res.answer.push({ 'name': question.name, 'type': 'A', 'data': doc.ip });
 				}
 			} else {
 				res.responseCode = 3;
@@ -78,4 +63,4 @@ var server = dnsd.createServer(function(req, res) {
 	}
 });
 
-server.zone(config.domain, 'ns1.' + config.domain, 'us@' + config.domain, 'now', '2h', '30m', '2w', '1m').listen(process.env.PORT || config.dns.port, process.env.HOST || config.dns.host);
+server.zone(config.domain, 'ns1.' + config.domain, config.contact || 'contact@' + config.domain, 'now', '2h', '30m', '2w', '1m').listen(process.env.PORT || config.dns.port || 53, process.env.HOST || config.dns.host || '0.0.0.0');
