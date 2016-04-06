057341b6f99a378aa7b53b1d11b6803eb03fc8a0
Joe Biellik <joebiellik@gmail.com>
2016-04-06 21:00:06 +0100

    Show status results

--- public/main.js
+++ public/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-	
+	$.get('/status.json', function(data) {
+		$('#status #spinner').hide();
+
+		$('#status #results #dns').addClass(data.dns.error ? 'card-danger' : 'card-success');
+		$('#status #results #dns p span').text(data.dns.result[0]);
+
+		$('#status #results #ping').addClass(data.ping.error ? 'card-warning' : 'card-success');
+		$('#status #results #ping p span').text(data.http.error ? 'not ' : '');
+
+		$('#status #results #http').addClass(data.http.error ? 'card-danger' : 'card-success');
+		$('#status #results #http p span').text(data.http.result);
+
+		$('#status #results').show();
+	});
 });
