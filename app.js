4376f61bea0aadef2a7d35a363d2fa451527a9b3
Joe Biellik <joebiellik@gmail.com>
2016-04-07 16:37:11 +0100

    Disable x-powered-by header

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var app = express();
 var pages = require('./controllers/page');
 var users = require('./controllers/user');
 
+app.disable('x-powered-by');
 app.set('view engine', 'jade');
 
 app.locals.pretty = true;
