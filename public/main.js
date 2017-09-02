a3e8c3cd07fe3d58ca4cea90fe7e22e17cc0998b
Joe Biellik <joebiellik@gmail.com>
2017-09-02 18:20:39 +0100

    Cleanup and update markup and styles

--- public/main.js
+++ public/main.js
@@ -11,13 +11,13 @@ $(function() {
 
 		var resolveMatch = !data.dns.error && data.dns.result == ip;
 
-		$('#status #results #dns').addClass(!resolveMatch ? 'card-danger' : 'card-success');
+		$('#status #results #dns').addClass(!resolveMatch ? 'bg-danger' : 'bg-success');
 		$('#status #results #dns p').text(domain + (data.dns.error ? ' does not resolve' : ' resolves to ' + data.dns.result));
 
-		$('#status #results #ping').addClass(data.ping.error ? 'card-warning' : 'card-success');
+		$('#status #results #ping').addClass(data.ping.error ? 'bg-warning' : 'bg-success');
 		$('#status #results #ping p').text((resolveMatch ? domain : ip) + ' is ' + (data.ping.error ? 'not ' : '') + 'replying to pings');
 
-		$('#status #results #http').addClass(data.http.error ? 'card-danger' : 'card-success');
+		$('#status #results #http').addClass(data.http.error ? 'bg-warning' : 'bg-success');
 		$('#status #results #http p').text('http://' + (resolveMatch ? domain : ip) + (data.http.result ? ' returns status code ' + data.http.result : ' could not be reached'));
 
 		$('#status #results').show();
